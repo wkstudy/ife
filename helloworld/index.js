aac9e22b8a3fea91f3d950964d4741461ed234d4
wkstudy <1346948470@qq.com>
2018-04-27 17:48:05 +0800

    finish version1.0
    完成用parcel快速搭建开发san helloworld(完全不懂，结果有时候能出来，有时候出不来，都是一步一步试出来的)

---
+++ helloworld/index.js
@@ -0,0 +1,17 @@
+
+import san from 'san';
+
+
+var MyApp = san.defineComponent({
+    template: '<div class=\'hello\'>{{msg}}</div>',
+
+    initData: function () {
+        return {
+            msg: 'hello world'
+        };
+    }
+});
+
+
+var myApp = new MyApp();
+myApp.attach(document.body);
